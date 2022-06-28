865a12ee49d08f1fb68780ba03f7ac2c0156e7a8
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-28 14:40:52 -0500

    Implement Journal Page using the layout previously created.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,14 @@
+import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste corrupti laudantium, eveniet aperiam repudiandae debitis quia ea exercitationem asperiores nihil omnis optio eaque minus obcaecati, impedit dolor nemo nobis minima.
+      </Typography>
+      {/* NothinSelected */}
+      {/* NotePage */}
+    </JournalLayout>
   )
 }
