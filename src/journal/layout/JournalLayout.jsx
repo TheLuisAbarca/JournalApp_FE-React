fe9fd3cca5df213f26466beb997f7465d1cb3658
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-28 16:32:54 -0500

    Update JournalLayout using SideBar component.

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from '@mui/material'
-import { NavBar } from '../components/NavBar';
+import { Box, Toolbar } from '@mui/material'
+import { NavBar, Sidebar } from '../components';
 
 const drawerWidth = 240;
 
@@ -7,12 +7,12 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex'}}>
         <NavBar drawerWidth={ drawerWidth }/>
-        {/* Sidebar drawerWidth*/}
+        <Sidebar drawerWidth={ drawerWidth }/>
         <Box 
             component='main'
             sx={{ flexGrow: 1, p: 3 }}
         >
-            {/* Toolbar */}
+            <Toolbar />
 
             { children }
         </Box>
