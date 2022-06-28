2bab7786e9bf8b082742332d742bed7ee001727b
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-28 15:13:32 -0500

    Update Journal Page importing Navbar into it.

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material'
+import { NavBar } from '../components/NavBar';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex'}}>
-        {/* Navbar drawerWidth*/}
+        <NavBar drawerWidth={ drawerWidth }/>
         {/* Sidebar drawerWidth*/}
         <Box 
             component='main'
