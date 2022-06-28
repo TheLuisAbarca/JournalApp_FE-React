ebec729ed3bdaab81440562f3ecede277cc22505
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-28 17:02:02 -0500

    Update JournalPage with the NothingSelectedView component.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,14 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      { /* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste corrupti laudantium, eveniet aperiam repudiandae debitis quia ea exercitationem asperiores nihil omnis optio eaque minus obcaecati, impedit dolor nemo nobis minima.
-      </Typography>
-      {/* NothinSelected */}
+      </Typography> */}
+      <NothingSelectedView />
       {/* NotePage */}
     </JournalLayout>
   )
