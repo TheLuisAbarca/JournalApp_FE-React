5c378c4e76352f547b1cfa50a0664cf8793cb5c4
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-28 17:39:45 -0500

    Update NoteView component into Journal Page.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
@@ -8,8 +8,8 @@ export const JournalPage = () => {
       { /* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste corrupti laudantium, eveniet aperiam repudiandae debitis quia ea exercitationem asperiores nihil omnis optio eaque minus obcaecati, impedit dolor nemo nobis minima.
       </Typography> */}
-      <NothingSelectedView />
-      {/* NotePage */}
+      { /* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
