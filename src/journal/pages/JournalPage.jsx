08357f6ab1b39e4883ce18c9b3b2ffbc01f3797f
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-29 10:21:30 -0500

    Add a floating button to ADD stuff.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,7 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
@@ -8,8 +9,21 @@ export const JournalPage = () => {
       { /* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste corrupti laudantium, eveniet aperiam repudiandae debitis quia ea exercitationem asperiores nihil omnis optio eaque minus obcaecati, impedit dolor nemo nobis minima.
       </Typography> */}
-      { /* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      { /* <NoteView /> */ }
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          '.hover': { backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   )
 }
