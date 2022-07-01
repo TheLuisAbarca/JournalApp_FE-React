3915b079ac9b8a69ddcc114fa68229547ad35217
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-01 17:04:10 -0500

    Add a basic state to checkinCredentials.

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
    name: 'auth',
    initialState: {
-       status: 'checking', // not-authenticated , authenticated
+       status: 'not-authenticated', // checking, not-authenticated , authenticated
        uid: null,
        email: null,
        displayName: null,
@@ -18,7 +18,7 @@ export const authSlice = createSlice({
 
         },
         checkinCredentials: (state) => {
-
+            state.status = 'checking';
         }
    }
 });
