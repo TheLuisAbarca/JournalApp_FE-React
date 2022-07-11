0f73ff87cf566b85f8c63cd0ac1042963e60cf96
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-11 17:21:33 -0500

    Implement a new function that change the state and began the process of creation of user in DB.

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { signInWithGoogle } from "../../firebase/providers";
+import { registerUserWithEmailPassword, signInWithGoogle } from "../../firebase/providers";
 import { checkinCredentials, logout, login } from "./";
 export const checkingAuthentication = ( email, password ) => { 
     return async( dispatch ) => {
@@ -12,5 +12,13 @@ export const startGoogleSignIn = () => {
         const result = await signInWithGoogle();
         if (!result.ok) return dispatch( logout( result.errorMessage) );
         dispatch( login(result) );
+    }
+}
+
+export const startCreatingUserWithEmailPassword = ({email, password, displayName}) => {
+    return async( dispatch ) => {
+        dispatch( checkinCredentials() );
+        const result = await registerUserWithEmailPassword({ email, password, displayName});
+        console.log(result);
     }
 }
