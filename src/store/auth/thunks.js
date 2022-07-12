3e351585b52b3cef80540bd265c8d2ac04bb4539
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-12 16:17:10 -0500

    Implement thunk for login with email and password.

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { registerUserWithEmailPassword, signInWithGoogle } from "../../firebase/providers";
+import { registerUserWithEmailPassword, signInWithGoogle, loginWithEmailPassword } from "../../firebase/providers";
 import { checkinCredentials, logout, login } from "./";
 export const checkingAuthentication = ( email, password ) => { 
     return async( dispatch ) => {
@@ -21,5 +21,14 @@ export const startCreatingUserWithEmailPassword = ({email, password, displayName
         const { ok, uid, photoURL, errorMessage } = await registerUserWithEmailPassword({ email, password, displayName});
         if ( !ok ) return dispatch(logout({ errorMessage }));
         dispatch( login({ uid, displayName, email, photoURL }))
+    }
+}
+
+export const startLoginWithEmailPassword = ({ email, password }) => {
+    return async( dispatch ) => {
+        dispatch( checkinCredentials() );
+        const result = await loginWithEmailPassword({ email, password });
+        if (!result.ok) return dispatch( logout( result ) );
+        dispatch( login(result) ); 
     }
 }
