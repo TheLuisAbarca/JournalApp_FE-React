57fa6b572e626d84736a5a95ce87bc5dd927fb99
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-12 17:01:00 -0500

    Use the spinner component as a default state of the app.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,17 @@
+import { useSelector } from "react-redux"
 import { Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
+import { CheckingAuth } from "../ui"
 
 export const AppRouter = () => {
+
+  const { status } = useSelector( state => state.auth );
+
+  if ( status === 'checking' ) {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
             {/* Login and Register*/}
