1c52af921d3e36d3944aea403e00fb25fe555f09
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-12 17:22:59 -0500

    Fix Grid container.

--- src/ui/components/CheckingAuth.jsx
+++ src/ui/components/CheckingAuth.jsx
@@ -10,7 +10,7 @@ export const CheckingAuth = () => {
         justifyContent="center"
         sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
         >
-            <Grid item
+            <Grid container
               direction='row'
               justifyContent='center'
             >
