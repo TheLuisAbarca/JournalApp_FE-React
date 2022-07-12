095d86e4d5e50aa23e26700602fd16780cc7dd92
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-12 17:47:50 -0500

    Clean code on AppRouter.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,12 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
-import { FirebaseAuth } from "../firebase/config";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { CheckingAuth } from "../ui";
-import { logout, login } from "../store/auth";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
 
-  const { status } = useSelector( state => state.auth );
-
-  const dispatch = useDispatch();
-
-  useEffect( () => {
-    onAuthStateChanged( FirebaseAuth, async( user )=> {
-      if (!user) return dispatch( logout());
-      const { uid, email, displayName, phoneNumber } = user;
-      dispatch( login({ uid, email, displayName, phoneNumber }));
-    } );
-  }, [])
+  const status = useCheckAuth();
 
   if ( status === 'checking' ) {
     return <CheckingAuth />
