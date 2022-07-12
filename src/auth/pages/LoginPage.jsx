45189c71ddddaa12282088466eab035483f7723c
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-12 16:18:37 -0500

    Use the thunk for login with email and password on login page.

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,16 +1,16 @@
 import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Alert, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
-import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
+import { startGoogleSignIn, startLoginWithEmailPassword } from '../../store/auth';
 
 
 export const LoginPage = () => {
 
-  const {status} =useSelector( state => state.auth );
+  const { status, errorMessage } =useSelector( state => state.auth );
 
   const dispatch = useDispatch();
 
@@ -24,7 +24,7 @@ export const LoginPage = () => {
   const onSubmit = (event) => {
     event.preventDefault();
     console.log({ email, password });
-    dispatch( checkingAuthentication() );
+    dispatch( startLoginWithEmailPassword({email, password}) );
   }
 
   const onGoogleSignIn = () => {
@@ -57,6 +57,20 @@ export const LoginPage = () => {
                value={ password }
                onChange={ onInputChange }
             />
+          </Grid>
+          <Grid 
+            container
+            display={ !!errorMessage ? '': 'none' }
+            sx={{ mt: 1 }}
+          >
+            <Grid 
+              item
+              xs={ 12 }
+            >
+              <Alert severity='error'>
+                { errorMessage }
+              </Alert>
+            </Grid>
           </Grid>
           <Grid container spacing={ 2 } sx={{mb: 2 , mt: 1 }}>
             <Grid item xs={ 12 } sm={ 6 }>
