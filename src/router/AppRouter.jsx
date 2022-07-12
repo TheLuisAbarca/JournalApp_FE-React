3bfb719f10a665bd4e1832223b13f4b2c15f466f
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-12 17:23:50 -0500

    Protect routes with the states of the authentication.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,26 @@
-import { useSelector } from "react-redux"
-import { Route, Routes } from "react-router-dom"
-import { AuthRoutes } from "../auth/routes/AuthRoutes"
-import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { CheckingAuth } from "../ui"
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { FirebaseAuth } from "../firebase/config";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui";
+import { logout, login } from "../store/auth";
 
 export const AppRouter = () => {
 
   const { status } = useSelector( state => state.auth );
+
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    onAuthStateChanged( FirebaseAuth, async( user )=> {
+      if (!user) return dispatch( logout());
+      const { uid, email, displayName, phoneNumber } = user;
+      dispatch( login({ uid, email, displayName, phoneNumber }));
+    } );
+  }, [])
 
   if ( status === 'checking' ) {
     return <CheckingAuth />
@@ -14,10 +28,18 @@ export const AppRouter = () => {
 
   return (
     <Routes>
+            {
+              ( status === 'authenticated' )
+              ? <Route path="/*" element={ <JournalRoutes />}/>
+              : <Route path="/auth/*" element={ <AuthRoutes />}/>
+            }
+
+            <Route path='/*' element={ <Navigate to='/auth/login' /> } />
+
             {/* Login and Register*/}
-            <Route path="/auth/*" element={ <AuthRoutes />}/>
+            { /* <Route path="/auth/*" element={ <AuthRoutes />}/> */}
             {/* JournalApp*/}
-            <Route path="/*" element={ <JournalRoutes />}/>
+            { /* <Route path="/*" element={ <JournalRoutes />}/> */ }
     </Routes>
   )
 }
