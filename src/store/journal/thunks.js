d3cbe5e2131c47c994de00a8607ae0b5df846b86
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-14 16:37:26 -0500

    Implement thunks for journal reducers.

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,25 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { CloudFirestore } from '../../firebase/config'
+import { addNewEmptyNote, setActiveNote } from "./journalSlice";
+
+export const startNewNote = () => {
+    return async( dispatch, getState ) => {
+        
+        const { uid } = getState().auth;
+        // uid
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime()
+        }
+
+        const newDoc = doc( collection( CloudFirestore, `${uid}/journal/notes` ));
+        const setDocAnsw = await setDoc( newDoc, newNote );
+
+        newNote.id = newDoc.id;
+
+        // dispatch
+        dispatch( addNewEmptyNote( newNote ) );
+        dispatch( setActiveNote( newNote ));
+    }
+}
