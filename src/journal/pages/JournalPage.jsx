0db995e2f9960a16a6cff558f7eec5a73de4a65f
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-14 17:10:04 -0500

    Use the actions implemented previously into the Journal Page.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,17 @@
+import { useDispatch } from "react-redux";
 import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { AddOutlined } from "@mui/icons-material";
+import { startNewNote } from "../../store/journal";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() );
+  }
+
   return (
     <JournalLayout>
       { /* <Typography>
@@ -12,6 +20,7 @@ export const JournalPage = () => {
       <NothingSelectedView />
       { /* <NoteView /> */ }
       <IconButton
+        onClick={ onClickNewNote }
         size='large'
         sx={{
           color: 'white',
