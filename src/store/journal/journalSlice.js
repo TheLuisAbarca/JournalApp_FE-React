7b106b1743700ddaad4b86829c042244f7df4e8e
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-15 16:46:57 -0500

    Implement the content of setNotes in the slice file.

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -27,7 +27,7 @@ export const journalSlice = createSlice({
             state.active = action.payload;
        },
        setNotes: (state, action ) => {
-        
+            state.notes = action.payload;
        },
        setSaving: ( state ) => {
         
