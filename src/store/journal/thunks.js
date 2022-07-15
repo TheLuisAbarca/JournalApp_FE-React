a345db1b34ffd8f1a31fc418931d166547372e59
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-15 12:04:45 -0500

    Use the new action previously implemented in the thunks file.

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,9 +1,10 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { CloudFirestore } from '../../firebase/config'
-import { addNewEmptyNote, setActiveNote } from "./journalSlice";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
 
 export const startNewNote = () => {
     return async( dispatch, getState ) => {
+        dispatch( savingNewNote() );
         
         const { uid } = getState().auth;
         // uid
