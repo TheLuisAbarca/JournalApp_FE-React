6f59b14380ee5533f7f98701b54ff5da01717140
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-15 16:42:51 -0500

    Implementation of a fetcher helper to get notes.

---
+++ src/helpers/loadNotes.js
@@ -0,0 +1,18 @@
+import { Co2Sharp } from "@mui/icons-material";
+import { collection, getDocs } from "firebase/firestore/lite";
+import { CloudFirestore } from "../firebase/config";
+
+export const loadNotes = async( uid= '') => {
+    if ( !uid ) throw new Error('User UID does not exist.');
+
+    const collectionRef = collection( CloudFirestore, `${ uid }/journal/notes`);
+    const docs = await getDocs(collectionRef);
+
+    const notes = [];
+    docs.forEach( doc => {
+        notes.push({id: doc.id, ...doc.data()});
+    });
+
+    console.log(notes);
+    return notes;
+}
