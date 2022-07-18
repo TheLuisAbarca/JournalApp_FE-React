1f4e1f5181d3f7e8d30b6f6801a5128d71d4c561
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-18 12:38:47 -0500

    Implement reload of state into form once something change.

--- src/hooks/useForms.js
+++ src/hooks/useForms.js
@@ -8,6 +8,10 @@ export const useForm = ( initialForm = {}, formValidations = {} ) => {
     useEffect(() => {
         createValidators();
     }, [formState]);
+
+    useEffect(() => {
+        setFormState( initialForm );
+    }, [ initialForm ]);
     
     const isFormValid = useMemo( () => {
         for (const formValue of Object.keys( formValidation )) {
