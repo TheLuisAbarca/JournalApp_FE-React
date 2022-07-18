b735689c65b578023909e426009fcb8be71479ff
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-18 16:29:04 -0500

    Use the action to save the note in redux and Firestore.

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,18 +1,28 @@
 import { SaveOutlined } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography } from '@mui/material'
-import { useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import { useEffect, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useForm } from '../../hooks'
+import { setActiveNote, startSaveNote } from '../../store/journal'
 import { ImageGallery } from '../components'
 
 export const NoteView = () => {
 
+    const dispatch = useDispatch();
     const { active:note } = useSelector( state => state.journal );
     const { body, title, date, onInputChange, formState } = useForm( note );
     const dateString = useMemo( () => {
         const newDate = new Date( date );
         return newDate.toUTCString();
     }, [date]);
+
+    useEffect(() => {
+      dispatch( setActiveNote(formState) );
+    }, [formState])
+    
+    const onSaveNote = () => {
+        dispatch( startSaveNote() );
+    }
 
   return (
     <Grid container
@@ -26,7 +36,11 @@ export const NoteView = () => {
             <Typography fontSize={ 39 } fontWeight='light'>{ dateString }</Typography>
         </Grid>
         <Grid item>
-            <Button color="primary" sx={{ padding: 2 }}>
+            <Button
+                onClick={ onSaveNote }
+                color="primary"
+                sx={{ padding: 2 }}
+            >
                 <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
                 Save
             </Button>
