d521549a6db874811c808f07c04ad454bb4034a6
TheLuisAbarca <arclaav.adm@gmail.com>
2022-07-27 09:50:10 -0500

    Implement preparation for upload files to cloudinary.

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,5 +1,6 @@
-import { SaveOutlined } from '@mui/icons-material';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import { SaveOutlined, UploadOutlined } from '@mui/icons-material';
+import { Button, Grid, IconButton, TextField, Typography } from '@mui/material';
+import { useRef } from 'react';
 import { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Swal from 'sweetalert2';
@@ -11,12 +12,14 @@ import { ImageGallery } from '../components';
 export const NoteView = () => {
 
     const dispatch = useDispatch();
-    const { active:note, messageSaved } = useSelector( state => state.journal );
+    const { active:note, messageSaved, isSaving } = useSelector( state => state.journal );
     const { body, title, date, onInputChange, formState } = useForm( note );
     const dateString = useMemo( () => {
         const newDate = new Date( date );
         return newDate.toUTCString();
     }, [date]);
+
+    const fileInputRef = useRef();
 
     useEffect(() => {
       dispatch( setActiveNote(formState) );
@@ -37,6 +40,11 @@ export const NoteView = () => {
         dispatch( startSaveNote() );
     }
 
+    const onFileInputChange = ({ target }) => {
+        if(target.files === 0) return;
+        console.log("Uploading Files...");
+        // dispatch( startUploadingFiles( target.files));
+    }
   return (
     <Grid container
         className='animate__animated animate__fadeIn animate__faster'
@@ -49,7 +57,25 @@ export const NoteView = () => {
             <Typography fontSize={ 39 } fontWeight='light'>{ dateString }</Typography>
         </Grid>
         <Grid item>
+
+            <input 
+                type="file"
+                multiple
+                ref={ fileInputRef }
+                onChange={ onFileInputChange }
+                style={{ display: 'none'}}
+            />
+
+            <IconButton
+                color="primary"
+                disabled={ isSaving }
+                onClick={ () => fileInputRef.current.click() }
+            >
+                <UploadOutlined />
+            </IconButton>
+
             <Button
+                disabled={ isSaving }
                 onClick={ onSaveNote }
                 color="primary"
                 sx={{ padding: 2 }}
